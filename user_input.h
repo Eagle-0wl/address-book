dbf2da46281c807b2ae5461363a9977d2af36102
Eagle-0wl <71759598+Eagle-0wl@users.noreply.github.com>
2021-10-12 13:35:14 +0300

    Delete user_input.h

--- user_input.h
+++
@@ -1,8 +0,0 @@
-//clears console from leftover input
-void clear_cosole();
-
-// Function gets user input string and removes new line from it
-void get_user_input (char *name,int lenght);
-
-//gets location where to insert contact from user
-int get_index();
